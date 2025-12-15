a78307399de1f15b76040d5e0ab91335360077a9
Ragul Akash V <145693911+ragulakash@users.noreply.github.com>
2025-12-15 22:39:08 +0530

    Update ChatPage.js

--- frontend/src/ChatPage.js
+++ frontend/src/ChatPage.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://e-rooms.onrender.com/");
 
 function ChatPage({ room, username }) {
   const [messages, setMessages] = useState([]);
